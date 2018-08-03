be82ff2a8306fc52c4a618e4ec6b7038c38e4471
chadkai <40427223+chadkai@users.noreply.github.com>
2018-08-03 21:28:11 +0100

    Update ps1c.py

--- ps1c.py
+++ ps1c.py
@@ -1,12 +1,11 @@
 # Inputs
-annualSalary = 300000
+annualSalary = float(input("Input your current annual salary; "))
 
 # Defined variables
 semiAnnualRaise = .07
 currentSavings = 0
 r = 0.04/12
 month = 0
-epsilon = 100
 
 # Counter
 steps = 0
@@ -20,22 +19,40 @@ guessRate = (highRate+lowRate)/2
 downPayment = 250000
 monthlySalary = annualSalary/12
 
-while abs(currentSavings - downPayment) > epsilon:
+found = False
+
+while abs(currentSavings - downPayment) >= 100.0:
+    # Establishing variable
     monthlyAmountSaved = monthlySalary * guessRate
+    # While loop to find total current savings under the current guessRate iteration
     while month < 36:
         currentSavings = (currentSavings + monthlyAmountSaved) * (1 + r)
         month += 1
         if month % 6 == 0:
             monthlyAmountSaved = monthlyAmountSaved * (1 + semiAnnualRaise)
-    print(currentSavings)
+
+    # Bisection
     if currentSavings < downPayment:
         lowRate = guessRate
     else:
         highRate = guessRate
+
+    # Counter
+    steps += 1
+
+    # Breaking loop when currentSavings is in range or when it is not possible to save
+    if int(currentSavings) in range(249900, 250100):
+        break
+    elif steps == 36:
+        print("Not possible to save with your current salary.")
+        break
+
+    # Resetting values
     currentSavings = 0
     month = 0
     guessRate = (highRate + lowRate) / 2
-    steps += 1
-    if steps == 36:
-        break
-    print(guessRate)
+    # Adding steps to the counter
+
+print("Recommended savings rate: ", guessRate)
+print("Number of steps in bisection method: ", steps)
+print(steps)
